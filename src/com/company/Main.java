e5321efdf8aba0f8aaa5460957ead2d6a9474199
Deyas01 <99634454+Deyas01@users.noreply.github.com>
2022-06-22 15:16:06 -0700

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 	for (int i=0; i<5; i++){
         System.out.println("Hello! Am I a Car?");
         System.out.println("Hello people!");
+        System.out.println("\"New update\"");
     }
     }
 }
